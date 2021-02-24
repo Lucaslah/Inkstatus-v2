269a2f96d7ab45f69d0cb21591c6a46832e4609e
Lucaslah <lucas.petrino@spycampllc.ml>
2021-02-25 12:45:01 +1300

    Finsished Building the Express Server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-export const n: number = 5;
-console.log(n);
+import { port } from "./config";
+
+const path = require("path");
+
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render(path.resolve(`views/index.ejs`), {
+        test: '123'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server Running at: http://localhost:${port}`);
+});
