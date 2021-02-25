6f6542469668f95fd6cf0b63c16216acc3d4e296
Lucaslah <lucas.petrino@spycampllc.ml>
2021-02-25 14:41:46 +1300

    Finished Backend & Started on Front End

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { port } from "./config";
 
 const path = require("path");
+const os = require("os")
+const ms = require('ms');
 
 const express = require('express')
 const app = express()
@@ -8,8 +10,31 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+
+    // Get MEM Info:
+var total_memory = os.totalmem();
+var total_mem_in_kb = total_memory / 1024;
+var total_mem_in_mb = total_mem_in_kb / 1024;
+var total_mem_in_gb = total_mem_in_mb / 1024;
+
+total_mem_in_kb = Math.floor(total_mem_in_kb);
+total_mem_in_mb = Math.floor(total_mem_in_mb);
+total_mem_in_gb = Math.floor(total_mem_in_gb);
+
+total_mem_in_mb = total_mem_in_mb % 1024;
+total_mem_in_kb = total_mem_in_kb % 1024;
+total_memory = total_memory % 1024;
+
+const core = os.cpus()[0];
+
     res.render(path.resolve(`views/index.ejs`), {
-        test: '123'
+        totalRam: `${total_mem_in_gb + " GB "}`,
+        platform: process.platform,
+        CPU_model: core.model,
+        CPU_speed: `${core.speed}Mhz`,
+        CUP_cores: os.cpus().length,
+        sysUptime: `${ms(os.uptime() * 1000, { long: true })}`
+
     });
 });
 
